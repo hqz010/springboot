2dadf7a295bfa9f7c5e6cd80bee2b197d25e0dd8
hqz010 <6285767@qq.com>
2018-06-07 15:23:06 +0800

    增加注解

--- src/main/java/com/hqz/springboot/service/UserService.java
+++ src/main/java/com/hqz/springboot/service/UserService.java
@@ -4,6 +4,9 @@ import com.hqz.springboot.model.User;
 
 import java.util.List;
 
+/**
+ * create by jason huang
+ */
 public interface UserService {
     List<User> findAllUser(int pageNum, int pageSize);
 }
